eb74fe8fb731f0370dbeecaffc9749f739a03029
Pedro <mendes.pmt@gmail.com>
2020-12-07 23:22:43 +0000

    Day 6 part 2

--- Assets/Day6/Day6.cs
+++ Assets/Day6/Day6.cs
@@ -1,32 +1,98 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Day6 : MonoBehaviour
 {
+    [Serializable]
+    private class PassengerAnswers
+    {
+        public HashSet<char> Answers = new HashSet<char>();
+
+        public PassengerAnswers(string answers)
+        {
+            char[] thisPersonAnswers = answers.ToCharArray();
+
+            foreach (char answer in answers)
+            {
+                Answers.Add(answer);
+            }
+        }
+
+        public bool Answered(char answer)
+        {
+            return Answers.Contains(answer);
+        }
+
+        public void ParseUniqueAnwswers(ref HashSet<char> answered)
+        {
+            foreach(char answer in Answers)
+            {
+                answered.Add(answer);
+            }
+        }
+    }
+
+    [Serializable]
+    private class GroupAnswers
+    {
+        public List<PassengerAnswers> Passengers = new List<PassengerAnswers>();
+
+        public void AddPassenger(string answers)
+        {
+            Passengers.Add(new PassengerAnswers(answers));
+        }
+
+        public int GetValidAnswers()
+        {
+            HashSet<char> firstPassengerAnswers = Passengers[0].Answers;
+            int totalCount = 0;
+
+            foreach (char answer in firstPassengerAnswers)
+            {
+                int count = 0;
+                Passengers.ForEach(p => count += p.Answered(answer) ? 1 : 0);
+
+                if (count == Passengers.Count)
+                {
+                    totalCount++;
+                }
+            }
+
+            return totalCount;
+        }
+
+        public int GetUnitqueAnsweres()
+        {
+            HashSet<char> uniques = new HashSet<char>();
+
+            Passengers.ForEach(p => p.ParseUniqueAnwswers(ref uniques));
+
+            return uniques.Count;
+        }
+    }
+
     [SerializeField]
     private TextAsset m_Input = null;
 
-    private List<HashSet<char>> m_Answers = new List<HashSet<char>>();
+    private List<GroupAnswers> m_GroupAnswers = new List<GroupAnswers>();
 
     void Start()
     {
         string input = m_Input.text;
         Queue<string> inputLines = new Queue<string>(input.Split('\n'));
 
+        int countValidAnswers = 0;
+        int countUniqueAnswers = 0;
+
         while (inputLines.Count > 0)
         {
-            HashSet<char> thisGroupAnswers = new HashSet<char>();
+            GroupAnswers groupAnswers = new GroupAnswers();         
 
             string newLine = inputLines.Dequeue().Replace("/n", "");
             while (!string.IsNullOrEmpty(newLine))
             {
-                char[] thisPersonAnswers = newLine.ToCharArray();
-
-                foreach(char answer in thisPersonAnswers)
-                {
-                    thisGroupAnswers.Add(answer);
-                }
+                groupAnswers.AddPassenger(newLine);
 
                 if (inputLines.Count > 0)
                 {
@@ -38,13 +104,14 @@ public class Day6 : MonoBehaviour
                 }
             }
 
-            Debug.Log("Number of answers: " + thisGroupAnswers.Count);
-            m_Answers.Add(thisGroupAnswers);
+            countUniqueAnswers += groupAnswers.GetUnitqueAnsweres();
+            Debug.Log("Number unique of answers: " + groupAnswers.GetUnitqueAnsweres());
+            countValidAnswers += groupAnswers.GetValidAnswers();
+            Debug.Log("Number valid of answers: " + groupAnswers.GetValidAnswers());
+            m_GroupAnswers.Add(groupAnswers);
         }
 
-        int count = 0;
-        m_Answers.ForEach(a => count += a.Count);
-
-        Debug.LogError("Total answers: " + count);
+        Debug.LogError("Total unique answers: " + countUniqueAnswers);
+        Debug.LogError("Total valid answers: " + countValidAnswers);
     }
 }
