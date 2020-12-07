98480417f062f4a51b6d4b32b3d511851a9a968c
Pedro <mendes.pmt@gmail.com>
2020-12-07 21:18:55 +0000

    Auto create new days

---
+++ Assets/Editor/CreateNewDay.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using UnityEditor.SceneManagement;
+using System;
+
+public class CreateNewDay : ScriptableWizard
+{
+    public int DayID = 0;
+
+    [MenuItem("Tools/Create New Day")]
+    static void CreateWizard()
+    {
+        DisplayWizard<CreateNewDay>("Create New Day", "Create");
+    }
+
+    private void OnWizardCreate()
+    {
+        string dayString = "Day" + DayID;
+
+        DirectoryInfo folder = new DirectoryInfo(Path.Combine(Application.dataPath, dayString));
+        
+        if (folder.Exists)
+        {
+            Debug.LogError("Day already exists : " + DayID);
+            return;
+        }
+
+        Directory.CreateDirectory(folder.ToString());
+
+        FileInfo sceneFile = new FileInfo(Path.Combine(folder.ToString(), dayString + ".unity"));
+        FileInfo templateScene = new FileInfo(Path.Combine(Application.dataPath, "Resources", "TemplateScene.unity"));
+        templateScene.CopyTo(sceneFile.ToString());
+
+        FileInfo scriptInfo = new FileInfo(Path.Combine(folder.ToString(), dayString + ".cs"));
+        TextAsset templateScript = Resources.Load("Template") as TextAsset;
+        string script = templateScript.text.Replace("Template", dayString);
+        File.WriteAllText(scriptInfo.ToString(), script);
+
+        AssetDatabase.Refresh();
+    }
+}
---
+++ Assets/Resources/Template.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Template : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset m_Input = null;
+
+    void Start()
+    {
+        string input = m_Input.text;
+        string[] inputLines = input.Split('\n');
+    }
+}
