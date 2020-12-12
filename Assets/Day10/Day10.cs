aa48f41ab94c78c0a1e584fed2250c80b108beb0
Pedro <mendes.pmt@gmail.com>
2020-12-12 22:31:37 +0000

    Day 10 part 2

--- Assets/Day10/Day10.cs
+++ Assets/Day10/Day10.cs
@@ -1,9 +1,103 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Day10 : MonoBehaviour
 {
+    private class TreeNode
+    {
+        public int Value;
+        public List<TreeNode> Childreen = new List<TreeNode>();
+
+        private long m_CountBranches = -1;
+
+        public TreeNode(int value, List<bool> nextNodes, ref Dictionary<int, TreeNode> allNodes)
+        {
+            Value = value;
+
+            allNodes[value] = this;
+
+            if (nextNodes == null || nextNodes.Count() == 0)
+            {
+                return;
+            }
+            if (nextNodes[0])
+            {
+                if (allNodes.TryGetValue(value + 1, out TreeNode node))
+                {
+                    Childreen.Add(node);
+                }
+                else
+                {
+                    List<bool> subNodes = nextNodes.Skip(1).ToList();
+                    Childreen.Add(new TreeNode(value + 1, subNodes, ref allNodes));
+                }
+            }
+
+            if (nextNodes.Count() <= 1)
+            {
+                return;
+            }
+            if (nextNodes[1])
+            {
+                if (allNodes.TryGetValue(value + 2, out TreeNode node))
+                {
+                    Childreen.Add(node);
+                }
+                else
+                {
+                    List<bool> subNodes = nextNodes.Skip(2).ToList();
+                    Childreen.Add(new TreeNode(value + 2, subNodes, ref allNodes));
+                }
+            }
+
+            if (nextNodes.Count() <= 2)
+            {
+                return;
+            }
+            if (nextNodes[2])
+            {
+                if (allNodes.TryGetValue(value + 3, out TreeNode node))
+                {
+                    Childreen.Add(node);
+                }
+                else
+                {
+                    List<bool> subNodes = nextNodes.Skip(3).ToList();
+                    Childreen.Add(new TreeNode(value + 3, subNodes, ref allNodes));
+                }
+            }
+        }
+
+        public bool IsLeaf
+        {
+            get
+            {
+                return Childreen == null || Childreen.Count == 0;
+            }
+        }
+
+        public long CountBranches()
+        {
+            if (m_CountBranches != -1)
+            {
+                return m_CountBranches;
+            }
+
+            if (IsLeaf)
+            {
+                m_CountBranches = 1;
+                return m_CountBranches;
+            }
+
+            m_CountBranches = 0;
+            Childreen.ForEach(node => m_CountBranches += node.CountBranches());
+            return m_CountBranches;
+        }
+    }
+
     [SerializeField]
     private List<TextAsset> m_Inputs = null;
 
@@ -58,7 +152,7 @@ public class Day10 : MonoBehaviour
         {
             int diff = chargers[i + 1] - chargers[i];
 
-            switch(diff)
+            switch (diff)
             {
                 case 1:
                     count1++;
@@ -76,5 +170,33 @@ public class Day10 : MonoBehaviour
         Debug.Log("Count 2 diff " + count2);
         Debug.Log("Count 3 diff " + count3);
         Debug.LogWarning("Result " + (count1 * count3));
+
+        //second part
+        bool[] availableChargers = new bool[chargers.Max() + 3];
+        availableChargers.Fill<bool>(false);
+
+        for (int i = 0; i < chargers.Count; i++)
+        {
+            availableChargers[chargers[i] - 1] = true;
+        }
+        // the extra charger
+        availableChargers[availableChargers.Length - 1] = true;
+
+        Dictionary<int, TreeNode> allNodes = new Dictionary<int, TreeNode>();
+
+        TreeNode chargersTree = new TreeNode(0, availableChargers.ToList(), ref allNodes);
+
+        Debug.LogWarning("Number of possibilities: " + chargersTree.CountBranches());
+    }
+}
+
+public static class ArrayExtensions
+{
+    public static void Fill<T>(this T[] originalArray, T with)
+    {
+        for (int i = 0; i < originalArray.Length; i++)
+        {
+            originalArray[i] = with;
+        }
     }
 }
