07e9a3d2492c3b5f088d79e76a38b79a722ad181
Pedro <mendes.pmt@gmail.com>
2020-12-15 23:54:24 +0000

    day 13 part 2 wip

--- Assets/Day13/Day13.cs
+++ Assets/Day13/Day13.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,10 +11,10 @@ public class Day13 : MonoBehaviour
     {
         for(int i = 0; i < m_Inputs.Count; i++)
         {
-            Debug.LogError("Running asset " + i);
+            Debug.LogError("Running asset " + m_Inputs[i].name);
             float startTime = Time.time;
             Run(i, m_Inputs[i]);
-            Debug.LogError("Finished asset " + i + " it took " + (Time.time - startTime)  + " seconds");
+            Debug.Log("Finished asset " + i + " it took " + (Time.time - startTime)  + " seconds");
         }
     }
 
@@ -50,5 +51,72 @@ public class Day13 : MonoBehaviour
 
         Debug.LogWarning("Departure at " + busDeparture + " on bus " + busID);
         Debug.LogWarning("Results: " + (busID * (busDeparture - minimumDeparture)));
+
+        List<ulong> busesInterval = new List<ulong>();
+        List<ulong> busesOffSets = new List<ulong>();
+        List<ulong> busesNextDeparture = new List<ulong>();
+
+        for (ulong i = 0; i < (ulong)buses.Length; i++)
+        {
+            if (buses[i] == "x")
+            {
+                continue;
+            }
+
+            ulong interval = ulong.Parse(buses[i]);
+
+            busesInterval.Add(interval);
+            busesOffSets.Add(i);
+        }
+
+        ulong time = 1;
+        
+        while (!CheckIntervals(time, busesInterval, busesOffSets))
+        {
+            time++;
+        }
+
+        Debug.LogWarning("Minimum time is: " + time);
+    }
+
+    private bool CheckIntervals(ulong time, List<ulong> intervals, List<ulong> offsets)
+    {
+        bool aligned = true;
+
+        for (int i = 0; i < intervals.Count; i++)
+        {
+            aligned &= ((time + offsets[i]) % intervals[i] == 0);
+        }
+
+        return aligned;
+    }
+
+    // worthless
+    // https://www.geeksforgeeks.org/lcm-of-given-array-elements/
+    // Returns LCM of array elements 
+    private ulong FindLCM(ulong[] arr)
+    {
+        // Initialize result 
+        ulong ans = arr[0];
+
+        // ans contains LCM of arr[0], ..arr[i] 
+        // after i'th iteration, 
+        for (int i = 1; i < arr.Length; i++)
+        {
+            ans = ((arr[i] * ans)) / (GBC(arr[i], ans));
+        }
+
+        return ans;
+    }
+
+    // Utility function to find 
+    // GCD of 'a' and 'b' 
+    private ulong GBC(ulong a, ulong b)
+    {
+        if (b == 0)
+        {
+            return a;
+        }
+        return GBC(b, a % b);
     }
 }
