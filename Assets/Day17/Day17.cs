ed13d5d67a272e8759fea45f06ca9c6800e39b66
Pedro Mendes <mendes.pmt@gmail.com>
2020-12-23 12:01:55 +0000

    Day 17 part 2

--- Assets/Day17/Day17.cs
+++ Assets/Day17/Day17.cs
@@ -24,6 +24,30 @@ public class Day17 : MonoBehaviour
         string input = inputAsset.text;
         string[] inputLines = input.Split('\n');
 
+        Run3D(inputLines);
+        Run4D(inputLines);
+    }
+
+    private void AddNeighbors3D(Vector3Int position, ref HashSet<Vector3Int> neighboors)
+    {
+        Vector3Int minPosition = position + new Vector3Int(-1, -1, -1);
+        Vector3Int maxPosition = position + new Vector3Int(1, 1, 1);
+
+        for (int xn = minPosition.x; xn <= maxPosition.x; xn++)
+        {
+            for (int yn = minPosition.y; yn <= maxPosition.y; yn++)
+            {
+                for (int zn = minPosition.z; zn <= maxPosition.z; zn++)
+                {
+                    Vector3Int cell = new Vector3Int(xn, yn, zn);
+                    neighboors.Add(cell);
+                }
+            }
+        }
+    }
+
+    private void Run3D(string[] inputLines)
+    {
         Dictionary<Vector3Int, bool> pocketDimension = new Dictionary<Vector3Int, bool>();
         HashSet<Vector3Int> neighboors = new HashSet<Vector3Int>();
         for (int y = 0; y < inputLines.Length; y++)
@@ -47,7 +71,7 @@ public class Day17 : MonoBehaviour
 
                 if (active)
                 {
-                    AddNeighbors(position, ref neighboors);
+                    AddNeighbors3D(position, ref neighboors);
                 }
             }
         }
@@ -58,11 +82,11 @@ public class Day17 : MonoBehaviour
         {
             Dictionary<Vector3Int, bool> newPocketDimension = new Dictionary<Vector3Int, bool>();
             HashSet<Vector3Int> newNeighboors = new HashSet<Vector3Int>();
-            
+
             foreach (Vector3Int cell in neighboors)
             {
                 Vector3Int minPosition = cell + new Vector3Int(-1, -1, -1);
-                Vector3Int maxPosition = cell + new Vector3Int( 1,  1,  1);
+                Vector3Int maxPosition = cell + new Vector3Int(1, 1, 1);
 
                 int countActiveNeighbors = 0;
 
@@ -100,7 +124,7 @@ public class Day17 : MonoBehaviour
 
                 if (newPocketDimension[cell])
                 {
-                    AddNeighbors(cell, ref newNeighboors);
+                    AddNeighbors3D(cell, ref newNeighboors);
                 }
             }
 
@@ -108,27 +132,119 @@ public class Day17 : MonoBehaviour
             neighboors = newNeighboors;
         }
 
-        Debug.LogWarning("Number of active cells: " + pocketDimension.Values.Count(cell => cell));
+        Debug.LogWarning("Number of 3D active cells: " + pocketDimension.Values.Count(cell => cell));
     }
 
-    private void AddNeighbors(Vector3Int position, ref HashSet<Vector3Int> neighboors)
+    private void AddNeighbors4D(Vector4 position, ref HashSet<Vector4> neighboors)
     {
-        Vector3Int minPosition = position + new Vector3Int(-1, -1, -1);
-        Vector3Int maxPosition = position + new Vector3Int(1, 1, 1);
+        Vector4 minPosition = position + new Vector4(-1f, -1f, -1f, -1f);
+        Vector4 maxPosition = position + new Vector4(1f, 1f, 1f, 1f);
 
-        for (int xn = minPosition.x; xn <= maxPosition.x; xn++)
+        for (int xn = (int)minPosition.x; xn <= (int)maxPosition.x; xn++)
         {
-            for (int yn = minPosition.y; yn <= maxPosition.y; yn++)
+            for (int yn = (int)minPosition.y; yn <= (int)maxPosition.y; yn++)
             {
-                for (int zn = minPosition.z; zn <= maxPosition.z; zn++)
+                for (int zn = (int)minPosition.z; zn <= (int)maxPosition.z; zn++)
                 {
-                    Vector3Int cell = new Vector3Int(xn, yn, zn);
-                    //if (Vector3Int.Distance(cell, position) == 1)
-                    //{
+                    for (int wn = (int)minPosition.w;  wn <= (int)maxPosition.w; wn++)
+                    {
+                        Vector4 cell = new Vector4(xn, yn, zn, wn);
                         neighboors.Add(cell);
-                    //}
+                    }
                 }
             }
         }
+    }
+
+    private void Run4D(string[] inputLines)
+    {
+        Dictionary<Vector4, bool> pocketDimension = new Dictionary<Vector4, bool>();
+        HashSet<Vector4> neighboors = new HashSet<Vector4>();
+        for (int y = 0; y < inputLines.Length; y++)
+        {
+            string line = inputLines[y];
+
+            if (string.IsNullOrEmpty(line))
+            {
+                continue;
+            }
+
+            char[] splitLine = line.ToCharArray();
+
+            for (int x = 0; x < splitLine.Length; x++)
+            {
+                bool active = splitLine[x] == '#';
+
+                Vector4 position = new Vector4(x, y, 0, 0);
+
+                pocketDimension[position] = active;
+
+                if (active)
+                {
+                    AddNeighbors4D(position, ref neighboors);
+                }
+            }
+        }
+
+        int maxIterations = 6;
+
+        for (int i = 0; i < maxIterations; i++)
+        {
+            Dictionary<Vector4, bool> newPocketDimension = new Dictionary<Vector4, bool>();
+            HashSet<Vector4> newNeighboors = new HashSet<Vector4>();
+
+            foreach (Vector4 cell in neighboors)
+            {
+                Vector4 minPosition = cell + new Vector4(-1, -1, -1, -1);
+                Vector4 maxPosition = cell + new Vector4(1, 1, 1, 1);
+
+                int countActiveNeighbors = 0;
+
+                for (int x = (int)minPosition.x; x <= (int)maxPosition.x; x++)
+                {
+                    for (int y = (int)minPosition.y; y <= (int)maxPosition.y; y++)
+                    {
+                        for (int z = (int)minPosition.z; z <= (int)maxPosition.z; z++)
+                        {
+                            for (int w = (int)minPosition.w; w <= (int)maxPosition.w; w++)
+                            {
+                                Vector4 neighboor = new Vector4(x, y, z, w);
+                                if (Vector4.Distance(cell, neighboor) == 0)
+                                {
+                                    continue;
+                                }
+
+                                if (pocketDimension.TryGetValue(neighboor, out bool active))
+                                {
+                                    countActiveNeighbors += active ? 1 : 0;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                bool activeState = false;
+                if (pocketDimension.ContainsKey(cell) && pocketDimension[cell])
+                {
+                    activeState = countActiveNeighbors == 2 || countActiveNeighbors == 3;
+                }
+                else
+                {
+                    activeState = countActiveNeighbors == 3;
+                }
+
+                newPocketDimension[cell] = activeState;
+
+                if (newPocketDimension[cell])
+                {
+                    AddNeighbors4D(cell, ref newNeighboors);
+                }
+            }
+
+            pocketDimension = newPocketDimension;
+            neighboors = newNeighboors;
+        }
+
+        Debug.LogWarning("Number of 4D active cells: " + pocketDimension.Values.Count(cell => cell));
     }
 }
